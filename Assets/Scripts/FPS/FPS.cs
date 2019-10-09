2d26708921a0ecc93cabc332f63a8a85e127b74e
zhengzhian <1552528597@qq.com>
2019-10-09 14:24:03 +0800

    添加显示FPS脚本

---
+++ Assets/Scripts/FPS/FPS.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPS : MonoBehaviour
+{
+
+    private float lastTime;
+    private float fps;
+
+    private float fpsUpdateInterval = 0.5f;     //FPS刷新间隔
+    private float lastUpdateTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void OnGUI()
+    {
+        GUILayout.TextField(string.Format("{0} FPS", fps.ToString("F0")));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.realtimeSinceStartup - lastUpdateTime > fpsUpdateInterval)
+        {
+            fps = 1 / (Time.realtimeSinceStartup - lastTime);
+            lastUpdateTime = Time.realtimeSinceStartup;
+        }
+        lastTime = Time.realtimeSinceStartup;
+    }
+}
